fff9839790b6cc1dd753baa65cb2b9863d48af89
Carsten <camueller19@outlook.de>
2015-06-17 22:30:02 +0200

    added prism, unity and mvvm Light

---
+++ DigitalAppaloosa.UserInterface/DigitalAppaloosaBootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DigitalAppaloosa.UserInterface
+{
+    public class DigitalAppaloosaBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            //Application.Current.RootVisual = (UIElement)this.Shell;
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+        }
+    }
+}
