da3c3608a5df251b6160b3eed5d094d449d827f7
Carsten <camueller19@outlook.de>
2015-06-25 22:08:43 +0200

    RegionAdapters

--- DigitalAppaloosa.Shared/Prism/RibbonRegionAdapter.cs
+++ DigitalAppaloosa.Shared/Prism/RibbonRegionAdapter.cs
@@ -35,102 +35,113 @@ namespace DigitalAppaloosa.Shared.Prism
                     // für jedes neue Item
                     foreach (object ribbonView in e.NewItems)
                     {
-                        // wenn es ein Ribbon ist - SPEZIALFALL
-                        if (ribbonView is Ribbon)
-                        {
-                            Ribbon rb = ribbonView as Ribbon;
-                            for (int i = rb.Items.Count - 1; i >= 0; i--)
-                            {
-                                if (rb.Items[i] is RibbonTab)
-                                {
-                                    // Umhängen aller Ribbontab in das Ribbon des Adapters
-                                    RibbonTab rt = (rb.Items[i] as RibbonTab);
-                                    rb.Items.Remove(rt);
-                                    ribbonRegionTarget.Items.Add(rt);
-                                }
-                            }
-                        }
-                        else
-                            // wenn es ein Ribbontab ist
-                            if (ribbonView is RibbonTab)
-                        {
-                            ribbonRegionTarget.Items.Add(ribbonView);
-                        }
-                        else
-                                // wenn es ein Ribbonbutton ist
-                                if (ribbonView is RibbonButton)
-                        {
-                            bool alreadyInserted = false;
-
-                            foreach (object ot in ribbonRegionTarget.Items)
-                            {
-                                // in das erste Ribbontab
-                                if (ot is RibbonTab && !alreadyInserted)
-                                {
-                                    foreach (object og in ((RibbonTab)ot).Items)
-                                    {
-                                        // in die erste Ribbon group
-                                        if (og is RibbonGroup)
-                                        {
-                                            ((RibbonGroup)og).Items.Add(ribbonView);
-                                            alreadyInserted = true;
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
-                        }
+                        AddRibbonViewToRegion(ribbonView);
                     }
                     break;
                 // entfernen von Ribboncontrols 
                 case NotifyCollectionChangedAction.Remove:
                     {
+                        //e.OldItems?
                         if (e.NewItems != null)
                             foreach (object ribbonView in e.NewItems)
                             {
-                                // wenn es ein Ribbon ist - SPEZIALFALL
-                                if (ribbonView is Ribbon)
-                                {
-                                    Ribbon rb = ribbonView as Ribbon;
-                                    for (int i = rb.Items.Count - 1; i >= 0; i--)
-                                    {
-                                        if (rb.Items[i] is RibbonTab)
-                                        {
-                                            RibbonTab rt = (rb.Items[i] as RibbonTab);
-                                            rb.Items.Remove(rt);
-                                            ribbonRegionTarget.Items.Remove(rt);
-                                        }
-                                    }
-                                }
-                                else
-                                    // wenn es ein Ribbontab ist
-                                    if (ribbonView is RibbonTab)
-                                {
-                                    ribbonRegionTarget.Items.Remove(ribbonView);
-                                }
-                                else
-                                        // wenn es ein Ribbonbutton ist
-                                        if (ribbonView is RibbonButton)
-                                {
-                                    // doesn't work - strange!
-                                    ((RibbonButton)ribbonView).Visibility = Visibility.Hidden;
-                                    // doesn't work - strange!
-
-                                    ribbonRegionTarget.Items.Remove(ribbonView);
-                                }
-                                else
-                                {
-                                    throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
-                                }
+                                RemoveRibbonViewToRegion(ribbonView);
                             }
                         break;
                     }
             }
         }
+
+        private void AddRibbonViewToRegion(object ribbonView)
+        {
+            // wenn es ein Ribbon ist - SPEZIALFALL
+            if (ribbonView is Ribbon)
+            {
+                Ribbon rb = ribbonView as Ribbon;
+                for (int i = rb.Items.Count - 1; i >= 0; i--)
+                {
+                    if (rb.Items[i] is RibbonTab)
+                    {
+                        // Umhängen aller Ribbontab in das Ribbon des Adapters
+                        RibbonTab rt = (rb.Items[i] as RibbonTab);
+                        rb.Items.Remove(rt);
+                        ribbonRegionTarget.Items.Add(rt);
+                    }
+                }
+            }
+            else
+                // wenn es ein Ribbontab ist
+                if (ribbonView is RibbonTab)
+            {
+                ribbonRegionTarget.Items.Add(ribbonView);
+            }
+            else
+                    // wenn es ein Ribbonbutton ist
+                    if (ribbonView is RibbonButton)
+            {
+                bool alreadyInserted = false;
+
+                foreach (object ot in ribbonRegionTarget.Items)
+                {
+                    // in das erste Ribbontab
+                    if (ot is RibbonTab && !alreadyInserted)
+                    {
+                        foreach (object og in ((RibbonTab)ot).Items)
+                        {
+                            // in die erste Ribbon group
+                            if (og is RibbonGroup)
+                            {
+                                ((RibbonGroup)og).Items.Add(ribbonView);
+                                alreadyInserted = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
+            }
+        }
+
+        private void RemoveRibbonViewToRegion(object ribbonView)
+        {
+            // wenn es ein Ribbon ist - SPEZIALFALL
+            if (ribbonView is Ribbon)
+            {
+                Ribbon rb = ribbonView as Ribbon;
+                for (int i = rb.Items.Count - 1; i >= 0; i--)
+                {
+                    if (rb.Items[i] is RibbonTab)
+                    {
+                        RibbonTab rt = (rb.Items[i] as RibbonTab);
+                        rb.Items.Remove(rt);
+                        ribbonRegionTarget.Items.Remove(rt);
+                    }
+                }
+            }
+            else
+                // wenn es ein Ribbontab ist
+                if (ribbonView is RibbonTab)
+            {
+                ribbonRegionTarget.Items.Remove(ribbonView);
+            }
+            else
+                    // wenn es ein Ribbonbutton ist
+                    if (ribbonView is RibbonButton)
+            {
+                // doesn't work - strange!
+                ((RibbonButton)ribbonView).Visibility = Visibility.Hidden;
+                // doesn't work - strange!
+
+                ribbonRegionTarget.Items.Remove(ribbonView);
+            }
+            else
+            {
+                throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
+            }
+        }        
 
         protected override IRegion CreateRegion()
         {
--- DigitalAppaloosa.Shared/Prism/RibbonTabRegionAdapter.cs
+++ DigitalAppaloosa.Shared/Prism/RibbonTabRegionAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Practices.Prism.Regions;
 using System.Collections.Specialized;
 using System.Windows.Controls.Ribbon;
+using System;
 
 namespace DigitalAppaloosa.Shared.Prism
 {
@@ -29,111 +30,141 @@ namespace DigitalAppaloosa.Shared.Prism
         {
             switch (e.Action)
             {
-                // Hinzufügen von Ribboncontrols
+
                 case NotifyCollectionChangedAction.Add:
                     // für jedes neue Item
                     foreach (object ribbonView in e.NewItems)
                     {
-                        //                // wenn es ein Ribbon ist - SPEZIALFALL
-                        //                if (ribbonView is Ribbon)
-                        //                {
-                        //                    Ribbon rb = ribbonView as Ribbon;
-                        //                    for (int i = rb.Items.Count - 1; i >= 0; i--)
-                        //                    {
-                        //                        if (rb.Items[i] is RibbonTab)
-                        //                        {
-                        //                            // Umhängen aller Ribbontab in das Ribbon des Adapters
-                        //                            RibbonTab rt = (rb.Items[i] as RibbonTab);
-                        //                            rb.Items.Remove(rt);
-                        //                            ribbonRegionTarget.Items.Add(rt);
-                        //                        }
-                        //                    }
-                        //                }
-                        //                else
-                        //                    // wenn es ein Ribbontab ist
-                        //                    if (ribbonView is RibbonTab)
-                        //                {
-                        //                    ribbonRegionTarget.Items.Add(ribbonView);
-                        //                }
-                        //                else
-                        //                        // wenn es ein Ribbonbutton ist
-                        //                        if (ribbonView is RibbonButton)
-                        //                {
-                        //                    bool alreadyInserted = false;
+                        AddRibbonViewToRegion(ribbonView);
 
-                        //                    foreach (object ot in ribbonRegionTarget.Items)
-                        //                    {
-                        //                        // in das erste Ribbontab
-                        //                        if (ot is RibbonTab && !alreadyInserted)
-                        //                        {
-                        //                            foreach (object og in ((RibbonTab)ot).Items)
-                        //                            {
-                        //                                // in die erste Ribbon group
-                        //                                if (og is RibbonGroup)
-                        //                                {
-                        //                                    ((RibbonGroup)og).Items.Add(ribbonView);
-                        //                                    alreadyInserted = true;
-                        //                                    break;
-                        //                                }
-                        //                            }
-                        //                        }
-                        //                    }
-                        //                }
-                        //                else
-                        //                {
-                        //                    throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
-                        //                }
                     }
                     break;
                 // entfernen von Ribboncontrols 
                 case NotifyCollectionChangedAction.Remove:
                     {
-                        //                if (e.NewItems != null)
-                        //                    foreach (object ribbonView in e.NewItems)
-                        //                    {
-                        //                        // wenn es ein Ribbon ist - SPEZIALFALL
-                        //                        if (ribbonView is Ribbon)
-                        //                        {
-                        //                            Ribbon rb = ribbonView as Ribbon;
-                        //                            for (int i = rb.Items.Count - 1; i >= 0; i--)
-                        //                            {
-                        //                                if (rb.Items[i] is RibbonTab)
-                        //                                {
-                        //                                    RibbonTab rt = (rb.Items[i] as RibbonTab);
-                        //                                    rb.Items.Remove(rt);
-                        //                                    ribbonRegionTarget.Items.Remove(rt);
-                        //                                }
-                        //                            }
-                        //                        }
-                        //                        else
-                        //                            // wenn es ein Ribbontab ist
-                        //                            if (ribbonView is RibbonTab)
-                        //                        {
-                        //                            ribbonRegionTarget.Items.Remove(ribbonView);
-                        //                        }
-                        //                        else
-                        //                                // wenn es ein Ribbonbutton ist
-                        //                                if (ribbonView is RibbonButton)
-                        //                        {
-                        //                            // doesn't work - strange!
-                        //                            ((RibbonButton)ribbonView).Visibility = Visibility.Hidden;
-                        //                            // doesn't work - strange!
+                        //e.OldItems?
+                        if (e.NewItems != null)
+                            foreach (object ribbonView in e.NewItems)
+                            {
+                                RemoveRibbonViewToRegion(ribbonView);
 
-                        //                            ribbonRegionTarget.Items.Remove(ribbonView);
-                        //                        }
-                        //                        else
-                        //                        {
-                        //                            throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
-                        //                        }
-                        //                    }
+                            }
                         break;
                     }
             }
         }
 
+
+
+        private void AddRibbonViewToRegion(object ribbonView)
+        {
+            //foreach (FrameworkElement element in e.NewItems)
+            //{
+            //    regionTarget.Items.Add(element);
+            //}
+
+            //// wenn es ein Ribbon ist - SPEZIALFALL
+            //if (ribbonView is Ribbon)
+            //{
+            //    Ribbon rb = ribbonView as Ribbon;
+            //    for (int i = rb.Items.Count - 1; i >= 0; i--)
+            //    {
+            //        if (rb.Items[i] is RibbonTab)
+            //        {
+            //            // Umhängen aller Ribbontab in das Ribbon des Adapters
+            //            RibbonTab rt = (rb.Items[i] as RibbonTab);
+            //            rb.Items.Remove(rt);
+            //            ribbonRegionTarget.Items.Add(rt);
+            //        }
+            //    }
+            //}
+            //else
+            //    // wenn es ein Ribbontab ist
+            //    if (ribbonView is RibbonTab)
+            //{
+            //    ribbonRegionTarget.Items.Add(ribbonView);
+            //}
+            //else
+            //        // wenn es ein Ribbonbutton ist
+            //        if (ribbonView is RibbonButton)
+            //{
+            //    bool alreadyInserted = false;
+
+            //    foreach (object ot in ribbonRegionTarget.Items)
+            //    {
+            //        // in das erste Ribbontab
+            //        if (ot is RibbonTab && !alreadyInserted)
+            //        {
+            //            foreach (object og in ((RibbonTab)ot).Items)
+            //            {
+            //                // in die erste Ribbon group
+            //                if (og is RibbonGroup)
+            //                {
+            //                    ((RibbonGroup)og).Items.Add(ribbonView);
+            //                    alreadyInserted = true;
+            //                    break;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
+            //}
+        }
+
+        private void RemoveRibbonViewToRegion(object ribbonView)
+        {
+            //foreach (UIElement elementLoopVariable in e.OldItems)
+            //{
+            //    var element = elementLoopVariable;
+            //    if (regionTarget.Items.Contains(element))
+            //    {
+            //        regionTarget.Items.Remove(element);
+            //    }
+            //}
+
+            //// wenn es ein Ribbon ist - SPEZIALFALL
+            //if (ribbonView is Ribbon)
+            //{
+            //    Ribbon rb = ribbonView as Ribbon;
+            //    for (int i = rb.Items.Count - 1; i >= 0; i--)
+            //    {
+            //        if (rb.Items[i] is RibbonTab)
+            //        {
+            //            RibbonTab rt = (rb.Items[i] as RibbonTab);
+            //            rb.Items.Remove(rt);
+            //            ribbonRegionTarget.Items.Remove(rt);
+            //        }
+            //    }
+            //}
+            //else
+            //    // wenn es ein Ribbontab ist
+            //    if (ribbonView is RibbonTab)
+            //{
+            //    ribbonRegionTarget.Items.Remove(ribbonView);
+            //}
+            //else
+            //        // wenn es ein Ribbonbutton ist
+            //        if (ribbonView is RibbonButton)
+            //{
+            //    // doesn't work - strange!
+            //    ((RibbonButton)ribbonView).Visibility = Visibility.Hidden;
+            //    // doesn't work - strange!
+
+            //    ribbonRegionTarget.Items.Remove(ribbonView);
+            //}
+            //else
+            //{
+            //    throw new ArgumentException("unsupported type " + ribbonView.GetType().Name);
+            //}
+        }
+
         protected override IRegion CreateRegion()
         {
             return new AllActiveRegion();
+            //return new SingleActiveRegion(); 
         }
     }
 }
