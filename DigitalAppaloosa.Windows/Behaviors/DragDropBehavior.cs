6157ff5968873c2f019f86a4a52a7be75c6b5435
Carsten <camueller19@outlook.de>
2015-10-21 22:30:03 +0200

    added Behaviors

---
+++ DigitalAppaloosa.Windows/Behaviors/DragDropBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using NLog;
+
+namespace DigitalAppaloosa.Windows.Behaviors
+{
+    public class DragDropBehavior : Behavior<ItemsControl>
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            var itemsControl = this.AssociatedObject;
+            itemsControl.PreviewMouseLeftButtonDown += ItemsControlPreviewMouseLeftButtonDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            var itemsControl = this.AssociatedObject;
+            itemsControl.PreviewMouseLeftButtonDown -= ItemsControlPreviewMouseLeftButtonDown;
+        }
+
+        private void ItemsControlPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            logger.Info("handled in DragDropBehavior ");
+        }
+    }
+}
